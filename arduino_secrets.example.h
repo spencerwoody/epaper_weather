9b9600f267681edb4d469839dbce93feec1a4738
spencerwoody <spencerwoody11@gmail.com>
2025-11-19 22:50:55 -0800

    arduino secrets example

---
+++ arduino_secrets.example.h
@@ -0,0 +1,16 @@
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+// WiFi credentials
+#define ssid "SSID"
+#define password "PASSWORD"
+
+// API keys
+#define GEOAPIFY_API_KEY  "YOUR_API_KEY"
+
+// Coordinates 
+// San Francisco 37.7749 N, 122.4194 W
+#define LATITUDE    37.7749
+#define LONGITUDE -122.4194
+
+#endif
